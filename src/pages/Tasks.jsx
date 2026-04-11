c3490fd0330ce4b4ce866a2cf06182bc8b377638
Kenny Portes <porteskenny@gmail.com>
2026-04-10 20:59:57 -0400

    Added functions on Tasks page with add, delete, and toggle function

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,7 +1,29 @@
 import { useState } from "react";
 
 function Tasks() {
+    const [tasks, setTasks] = useState([]);
+    const [filter, setFilter] = useState("all");
+
+    function addTask(text) {
+        setTasks([...tasks, { id: Date.now(), text, complete: false }]);
+    }
+
+     function deleteTask(id) {
+        setTasks(tasks.filter(task => task.id !== id));
+    }
+ 
+      function toggleTask(id) {
+    setTasks(tasks.map(task =>
+        task.id === id
+          ? { ...task, completed: !task.completed } 
+          : task
+      ));
+    }
+ 
 
 }
+
+     
+
 
 export default Tasks;
