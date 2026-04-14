7c92dc55d63199b12d5018713bb7bb4ea37fe02a
Kenny Portes <porteskenny@gmail.com>
2026-04-13 22:12:39 -0400

    Added all 4 form field in Contact file

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,16 +1,58 @@
 import { useState } from "react";
 
 function Contact() {
-   const [form, setForm] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    comment: "",
-   });
+    const [form, setForm] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        comment: "",
+    });
 
-   function handleChange(e) {
-    setForm({  ...form, [e.target.name]: e.target.value});
-   }
+    function handleChange(e) {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    }
+    return (
+        <>
+            <div>
+                <h2 style={{ textDecoration: "underline" }}>Contact Form</h2>
+            </div>
+            <div>
+                <br />
+                <input
+                    name="firstName"
+                    placeholder="First Name"
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <br />
+                <input
+                    name="lastName"
+                    placeholder="Last Name"
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <br />
+                <input
+                    name="email"
+                    placeholder="Email"
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <br />
+                <textarea
+                    name="comments"
+                    placeholder="Comments"
+                    rows="6"   
+                    cols="50"
+                    onChange={handleChange}
+                />
+            </div>
+        </>
+
+    );
 }
 
 export default Contact;
