53fcdcb9ed5f361c58efbe8ea6e051dd8869c31b
Kenny Portes <porteskenny@gmail.com>
2026-04-13 22:40:07 -0400

    Created a form for all my input fields to be submitted with a submit button. also an added and alert to let user know form was submitted.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,50 +5,69 @@ function Contact() {
         firstName: "",
         lastName: "",
         email: "",
-        comment: "",
+        comments: "",
     });
 
     function handleChange(e) {
         setForm({ ...form, [e.target.name]: e.target.value });
     }
+
+    function handleSubmit(e) {
+        e.preventDefault(); 
+        console.log("Form Data:", form);
+        alert(`Message sent by ${form.firstName}!`);
+    }
     return (
         <>
             <div>
                 <h2 style={{ textDecoration: "underline" }}>Contact Form</h2>
-            </div>
-            <div>
-                <br />
-                <input
-                    name="firstName"
-                    placeholder="First Name"
-                    onChange={handleChange}
-                />
-            </div>
-            <div>
-                <br />
-                <input
-                    name="lastName"
-                    placeholder="Last Name"
-                    onChange={handleChange}
-                />
-            </div>
-            <div>
-                <br />
-                <input
-                    name="email"
-                    placeholder="Email"
-                    onChange={handleChange}
-                />
-            </div>
-            <div>
-                <br />
-                <textarea
-                    name="comments"
-                    placeholder="Comments"
-                    rows="6"   
-                    cols="50"
-                    onChange={handleChange}
-                />
+
+                <form onSubmit={handleSubmit}>
+                <div className="w-25 mx-auto">
+                    <br />
+                    <input
+                        className="form-control"
+                        name="firstName"
+                        placeholder="First Name"
+                        value={form.firstName}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="w-25 mx-auto">
+                    <br />
+                    <input
+                        className="form-control"
+                        name="lastName"
+                        placeholder="Last Name"
+                        value={form.lastName}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="w-25 mx-auto">
+                    <br />
+                    <input
+                        className="form-control"
+                        name="email"
+                        type="email"
+                        placeholder="Email"
+                        value={form.email}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="w-50 mx-auto">
+                    <br />
+                    <textarea
+                        className="form-control"
+                        name="comments"
+                        placeholder="Comments"
+                        rows="6"
+                        cols="50"
+                        value={form.comments}
+                        onChange={handleChange}
+                    />
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
             </div>
         </>
 
